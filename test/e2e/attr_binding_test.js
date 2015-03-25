63cbc946eb8a3cb7404dc99551cf40ab95ef6221
e-ryu <e.ryu@astamuse.co.jp>
2015-03-25 16:27:42 +0900

    attr test is ok

---
+++ test/e2e/attr_binding_test.js
@@ -0,0 +1,33 @@
+var assert = require('assert');
+
+describe('attr binding tests', function() {
+    
+    var page = browser.url('http://localhost:9001/devstub/attr_binding.html');
+    
+    it("title test", function(done){
+      page.getTitle(function(err,title) {
+         assert.strictEqual(title,'attr_binding');
+      });
+      page.call(done);
+    });
+    
+    it("set attr test", function(done){
+      page.click("#set-value");
+      page.getAttribute("#target-table", "border", function(err, attr){
+        assert.strictEqual(attr,'1');
+      });
+      page.getAttribute("#target-table", "style", function(err, style){
+        assert.strictEqual(style,'width: 30%;');
+      });
+      page.getAttribute("#target-table", "class", function(err, cls){
+        var all = cls.split(" ");
+        assert.equal(all.indexOf("blue") >=0, true);
+        assert.equal(all.indexOf("height40") >=0, true);
+      });
+      page.getAttribute("#check-box", "checked", function(err, checked){
+        assert.equal(Boolean(checked), false);
+      });
+      page.call(done);
+    });
+    
+});
