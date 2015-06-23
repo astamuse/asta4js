c37cdff554163005e9c6ccc351af74731562ed6c
e-ryu <e.ryu@astamuse.co.jp>
2015-06-23 19:46:12 +0900

    a not bad proposal of how to organize client source

--- examples/tree/asta4js.js
+++ examples/tree/asta4js.js
@@ -19,14 +19,23 @@ $(function () {
     }; 
     
     initTreeData();
-     
-    $scope.snippet(".x-tree").bind($scope.data, {
-      tree:{
-        _meta_id: "tree",
-        _duplicator: "li",
+    
+    $scope.snippet("#tree-view").bind(".x-reset-btn", "click", function(){
+      initTreeData();
+    }).bind(".x-add-top-btn", "click", function(){
+      if($scope.data.tree === undefined || $scope.data.tree === null){//make sure it is not undefined
+        $scope.data.tree = [];
+      }
+      $scope.data.tree.push({
+        id: $scope.data.tree.length + 1,
+        title: "node" + ($scope.data.tree.length + 1),
+        nodes: []
+      });
+    });
+    
+    var itemControl = {
+      meta: {
         _item: {
-          _context: "a.btn",
-          title: ".x-title",
           nodes: {
             length: {
               _selector: ".x-toggle-btn@>[style:display=]",
@@ -34,6 +43,69 @@ $(function () {
                 return v ? "" : "none";
               }
             },
+            "@collapsed" :[
+              {
+                _virtual: true,
+                _value_ref: ".x-toggle-btn"
+              },
+              {
+                _virtual: true
+                _selector: ">.x-toggle-icon@>[class:glyphicon-chevron-right|glyphicon-chevron-down]",
+                _transform: function(v){
+                  return v ? "glyphicon-chevron-right" : "glyphicon-chevron-down"
+                }
+              },
+              {
+                _virtual: true,
+                _selector: ">.x-child-tree-root>ol@>[style:display=]",
+                _transform: function(v){
+                  return v ? "none" : ""
+                }
+              },
+            ]//end @collapsed
+          }
+        }//_item
+      },//meta
+      handler: function(snippet){
+        snippet.on("click", ".x-toggle-btn", function(){
+          var ref = Aj.getValueRef(this);
+          ref.setValue(!ref.getValue());
+        });
+      }
+    }
+    
+    var itemOp = {
+      meta: {
+        _item: {
+          _context: "a.btn"
+        },
+      },
+      handler: function(snippet){
+        snippet.on("click", ".x-remove-btn", function(){
+          Aj.getContext(this).getArrayAssistant(true).remove();
+        }).on("click", ".x-add-btn", function(){
+          var assistant = Aj.getContext(this).getArrayAssistant(true);
+          var item = assistant.getItem();
+          if(!Array.isArray(item.nodes)){
+            item.nodes = [];
+          }
+          item.nodes.push({
+            id: item.id * 10 + item.nodes.length,
+            title: item.title + "." + (item.nodes.length + 1),
+            nodes: []
+          });
+        })
+      }
+    }
+     
+    $scope.snippet(".x-tree").bind($scope.data, {
+      tree:{
+        _meta_id: "tree",
+        _duplicator: "li",
+        _merge: [itemControl.meta, itemOp.meta]
+        _item: {
+          title: ".x-title",
+          nodes: {
             _nest: {
               _root: ".x-tree",
               _child_root_parent: ".x-child-tree-root",
@@ -42,49 +114,7 @@ $(function () {
           },// nodes
         },
       }
-    }).on("click", ".x-remove-btn", function(){
-      Aj.getContext(this).getArrayAssistant(true).remove();
-    }).on("click", ".x-add-btn", function(){
-      var assistant = Aj.getContext(this).getArrayAssistant(true);
-      var item = assistant.getItem();
-      if(!Array.isArray(item.nodes)){
-        item.nodes = [];
-      }
-      item.nodes.push({
-        id: item.id * 10 + item.nodes.length,
-        title: item.title + "." + (item.nodes.length + 1),
-        nodes: []
-      });
-    }).on("click", ".x-toggle-btn", function(){
-      var btn = $(this);
-      var targetChildTree = btn.closest("li").find(".x-child-tree-root>ol");
-      var icon = btn.find(".x-toggle-icon");
-      if(icon.hasClass("glyphicon-chevron-down")){//collapse
-        icon.removeClass("glyphicon-chevron-down");
-        icon.addClass("glyphicon-chevron-right");
-        targetChildTree.css("display", "none");
-      }else{//expand
-        icon.removeClass("glyphicon-chevron-right");
-        icon.addClass("glyphicon-chevron-down");
-        targetChildTree.css("display", "");
-      }
-      
-    });
-    
-    $scope.snippet("#tree-view")
-          .bind(".x-reset-btn", "click", function(){
-            initTreeData();
-          })
-          .bind(".x-add-top-btn", "click", function(){
-            if($scope.data.tree === undefined || $scope.data.tree === null){//make sure it is not undefined
-              $scope.data.tree = [];
-            }
-            $scope.data.tree.push({
-              id: $scope.data.tree.length + 1,
-              title: "node" + ($scope.data.tree.length + 1),
-              nodes: []
-            });
-          });
+    }).engage(itemOp.handler)
+      .engage(itemControl.handler);
   });
-
 });
